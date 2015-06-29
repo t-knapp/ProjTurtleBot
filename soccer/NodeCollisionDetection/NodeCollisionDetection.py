927c0a63dc85e1925380780e730b316c1b804097
ROS2 <ROS2@ros.fh-bingen.de>
2015-06-29 11:51:06 +0200

    Fixed Threading Issues

--- soccer/NodeCollisionDetection/NodeCollisionDetection.py
+++ soccer/NodeCollisionDetection/NodeCollisionDetection.py
@@ -7,6 +7,7 @@ import argparse
 import rospy
 import numpy as np
 import time
+import threading
 
 import message_filters
 
@@ -29,7 +30,7 @@ class NodeCollisionDetection(object):
         rospy.init_node(name, anonymous=False)
         
         # Subscribe to NodeBallDetection
-        rospy.Subscriber("/soccer/movement", Twist, self.callbackMovement, queue_size=1)
+        self.subscribe()
         # Subscribe to Bumper Events
         rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, self.collisionHandler)
         # Subscribe to Referee Events
@@ -37,40 +38,51 @@ class NodeCollisionDetection(object):
         # Publisher to movement
         self.move = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
         
+        self.lock = threading.Lock()
         self.noCollision = True
         self.run = True
         
         r = rospy.Rate(5)
         while not rospy.is_shutdown() and self.run:
-            r.sleep()
+                r.sleep()
 
 
     def callbackMovement(self, data):
+        self.lock.acquire()
         if self.noCollision and self.run:
             self.move.publish(data)
+            print data
+        self.lock.release()
 
     def refereeHandler(self,data):
         print data
         self.run = data.data
 
     def collisionHandler(self, msg):
-        if msg.state != BumperEvent.PRESSED:
+        self.lock.acquire()
+        if self.noCollision:
             self.noCollision = False
-            t = Twist()
+            if msg.state != BumperEvent.PRESSED and self.run:
+                self.unsubscribe()
+                t = Twist()
+                t.linear.x = -self.LINEAR_SPEED
+                if msg.bumper == 0: # LEFT Bumper
+                    t.angular.z = self.ANGULAR_SPEED
+                if msg.bumper == 2: # RIGHT Bumper
+                    t.angular.z = -self.ANGULAR_SPEED
+                
+                for x in range(0,5):
+                    self.move.publish(t)
+                    time.sleep(0.1)
+                self.subscribe()
+            self.noCollision = True
+            self.lock.release()
 
-            t.linear.x = -self.LINEAR_SPEED
-            if msg.bumper == 0: # LEFT Bumper
-                t.angular.z = self.ANGULAR_SPEED
-            if msg.bumper == 2: # RIGHT Bumper
-                t.angular.z = -self.ANGULAR_SPEED
-            
-            for x in range(0,5):
-                self.move.publish(t)
-                time.sleep(0.01)
-
-        self.noCollision = True
-
-
+    def subscribe(self):
+        self.movement_sub = rospy.Subscriber("/soccer/movement", Twist, self.callbackMovement, queue_size=1)
+    
+    def unsubscribe(self):
+        self.movement_sub.unregister()
 
 if __name__ == '__main__':
     NodeCollisionDetection()
